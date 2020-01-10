edd9770b686340a6f6ff5545b58f771ffe7b2dde
Almar Klein <almar.klein@gmail.com>
2020-01-10 16:51:17 +0100

    tweak setup.py (pypi does not like the format of readme)

--- setup.py
+++ setup.py
@@ -6,18 +6,22 @@ from setuptools import find_packages, setup
 with open("python_shader/__init__.py") as fh:
     VERSION = re.search(r"__version__ = \"(.*?)\"", fh.read()).group(1)
 
+description = "Write modern GPU shaders in Python!"
+url = "https://github.com/almarklein/python-shader"
+
 setup(
     name="python-shader",
     version=VERSION,
-    description="Write modern GPU shaders in Python!",
+    description=description,
     packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
     python_requires=">=3.6.0",
     license=open("LICENSE").read(),
-    long_description=open("README.md").read(),
+    # long_description=open("README.md").read(),
+    long_description=description + "\n\n" + url,
     long_description_content_type="text/markdown",
     author="Almar Klein",
     author_email="almar.klein@gmail.com",
-    url="https://github.com/almarklein/python_shader",
+    url=url,
     zip_safe=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
