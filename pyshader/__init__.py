0a26b062e1131494d5cbe0838a02be995e61ca7a
Almar Klein <almar.klein@gmail.com>
2020-06-15 23:20:16 +0200

    bump version

--- pyshader/__init__.py
+++ pyshader/__init__.py
@@ -4,7 +4,7 @@ Write modern GPU shaders in Python!
 
 # flake8: noqa
 
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 version_info = tuple(map(int, __version__.split(".")))
 
 from ._coreutils import ShaderError
